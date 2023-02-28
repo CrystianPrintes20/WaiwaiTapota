54f9ed5936d4a71fccbec86f142dfb8bccf1e175
CrystianPrintes20 <crystianprintes.ufopa@gmail.com>
2023-02-28 15:00:03 -0300

    Fix: Updates project

--- src/components/banner/Banner2.js
+++ src/components/banner/Banner2.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { Container, Row, Col } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Badge,
+} from "reactstrap";
 import Image from "next/image";
-import bannerimg from "../../assets/aplicativoTapota/icones-logo-tapota/tapotaBranco.png";
+import bannerimg from "../../assets/aplicativoTapota/icones-logo-tapota/logoTipoBranco.png";
 
 const Banner2 = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="static-slider-head banner2">
       <Container>
@@ -17,12 +31,12 @@ const Banner2 = () => {
               Com o WaiwaiTapota, você tem traduções da lingua indigena Waiwai
               para o portugues.
             </h4>
-            <a
-              href="https://wrappixel.com/templates/nextkit-nextjs-free-uikit"
+            <Button
               className="btn btn-danger m-r-20 btn-md m-t-30 "
+              onClick={toggle}
             >
               Download App
-            </a>
+            </Button>
             {/* <Link href="/#coming">
               <a className="btn btn-md m-t-30  btn-outline-light ">
                 Upgrade To Pro
@@ -30,9 +44,23 @@ const Banner2 = () => {
             </Link> */}
           </Col>
           <Col lg="6" md="6" className="py-4">
-            <Image src={bannerimg} alt="hero banner" width={400} height={400} />
+            <Image src={bannerimg} alt="hero banner" width={450} height={450} />
           </Col>
         </Row>
+
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Baixe o nosso aplicativo</ModalHeader>
+          <ModalBody className="d-block">
+            <Badge href="../downloasApk/app-arm64-v8a-release.apk" className="mx-3">App 64-v8a</Badge>
+            <Badge href="../downloasApk/app-arm64-v8a-release.apk" className="mx-3">App 64-v8a</Badge>
+            <Badge href="../downloasApk/app-arm64-v8a-release.apk" className="mx-3">App 64-v8a</Badge>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
       </Container>
     </div>
   );
--- src/components/custom/sections/teamcomponent.js
+++ src/components/custom/sections/teamcomponent.js
@@ -31,30 +31,41 @@ const TeamComponent = () => {
             </Col>
           </Row>
           <Row className="m-t-30">
-            
-            <Carousel cols={4} rows={1} gap={4} loop  >
-                
-              <Carousel.Item style="
-    grid-template-columns: repeat(4, 50%);
-">
+            <Carousel
+              cols={4}
+              rows={1}
+              gap={4}
+              loop
+              responsiveLayout={[
+                {
+                  breakpoint: 1200,
+                  cols: 3,
+                },
+                {
+                  breakpoint: 990,
+                  cols: 2,
+                },
+              ]}
+              mobileBreakpoint={100}
+            >
+              <Carousel.Item style="grid-template-columns: repeat(4, 50%);">
                 <Col lg="12" md="6" className="m-b-30">
                   <Row className="no-gutters">
-                    <Col md="12" className="pro-pic t1" width={"auto"}>
+                    <Col md="12" className="col-md-12 pro-pic t1">
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="https://www.linkedin.com/in/crystian-printes-b052691b7/"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:crystianprintes.ufopa@gmail.com">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -62,8 +73,15 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 01</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">Crystian Printes</h5>
+                        <h6 className="subtitle">
+                          Possuo conhecimento em programação,
+                          principalmente focado no desenvolvimento frontend de
+                          aplicações web. Possuo habilidades com as linguagens
+                          de programação PHP e JavaScript, além de HTML5, CSS3,
+                          AJAX, Jquery, CURL, Nodejs, React, Nextjs e
+                          conhecimento em POO e MVC.
+                        </h6>
                       </div>
                     </Col>
                   </Row>
@@ -76,18 +94,17 @@ const TeamComponent = () => {
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="https://br.linkedin.com/in/marcos-vinicius-de-castro-printes-b22442201"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:mvprintes2@gmail.com">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -95,8 +112,13 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 02</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">
+                          Marcos Vinicius de Castro Printes
+                        </h5>
+                        <h6 className="subtitle">
+                          Sou um desenvolvedor de aplicações mobile tenho
+                          experiência com Java, C#, python e Flutter.
+                        </h6>
                       </div>
                     </Col>
                   </Row>
@@ -110,18 +132,17 @@ const TeamComponent = () => {
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="http://lattes.cnpq.br/1806104313800692"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:debeokjin@gmail.com">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -129,8 +150,17 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 03</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">
+                          Debora marciao dos santos
+                        </h5>
+                        <h6 className="subtitle">
+                          Artista visual, formade em Arqueologia pela
+                          Universidade Federal do Oeste do Pará (UFOPA).
+                          Atualmente é estudante de Design Gráfico na EBAC. Atua
+                          na MALUNGU-PA como designer e ilustradora, coordena o
+                          projeto Estúdio Taberna e trabalha diretamente com
+                          Monhangarypi Produções.
+                        </h6>
                       </div>
                     </Col>
                   </Row>
@@ -143,18 +173,17 @@ const TeamComponent = () => {
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4762839U8"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:camilajacome88@gmail.com">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -162,8 +191,25 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 04</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">
+                          Camila Pereira Jácome
+                        </h5>
+                        <h6 className="subtitle">
+                          Doutora em Arqueologia pelo Museu de Arqueologia e
+                          Etnologia da USP, com tese sobre encontros ontologicos
+                          entre a arqueologia e culturas indígenas na região do
+                          Rio Trombetas, Oriximiná, Pará. Participa desde 1999
+                          como colaboradora de pesquisas do Setor de Arqueologia ...
+                          {/* do Museu de História Natural e Jardim Botânico da
+                          Universidade Federal de Minas Gerais. Professora
+                          Adjunta no curso de Bacharelado em Arqueologia da
+                          Universidade Federal do Oeste do Pará (UFOPA). Tem
+                          experiência na área de Arqueologia Pré-colonial, com
+                          ênfase em tecnologia cerâmica, mas também tem formação
+                          em análise de arte rupestre e tecnologia lítica.
+                          Publicação e interesse sobre educação, arqueologias
+                          descolonais, arqueologia de gênero e feminismo. */}
+                        </h6>
                       </div>
                     </Col>
                   </Row>
@@ -172,22 +218,21 @@ const TeamComponent = () => {
               <Carousel.Item>
                 <Col className="m-b-30">
                   <Row className="no-gutters">
-                    <Col md="12" className="col-md-12 pro-pic t4">
+                    <Col md="12" className="col-md-12 pro-pic t5">
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="http://lattes.cnpq.br/3434022917410660"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:flavia.monteiro@ufopa.edu.br">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -195,8 +240,26 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 04</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">
+                          Flávia Pessoa Monteiro
+                        </h5>
+                        <h6 className="subtitle">
+                          Atualmente Professora do curso de bacharelado em
+                          Sistemas de Informação na Universidade Federal do
+                          Oeste do Pará (UFOPA), lotada no campus de Oriximiná,
+                          bem no coração da Amazônia. Doutora em Engenharia
+                          Elétrica na área de Sistemas de Energia pela ...
+                          {/* Universidade Federal do Pará no Programa de
+                          Pós-Graduação em Engenharia Elétrica - PPGEE, em 2019.
+                          Mestre em Engenharia Elétrica com área de concentração
+                          em Computação Aplicada com linha de pesquisa em
+                          Inteligência Computacional. Bacharel em Engenharia da
+                          Computação pelo Instituto de Ensino Superior da
+                          Amazônia (IESAM) no ano de 2014. Atua nas seguintes
+                          linhas: computação aplicada a sistemas de energia,
+                          tecnologias sociais, Data Science e aprendizagem de
+                          máquina. */}
+                        </h6>
                       </div>
                     </Col>
                   </Row>
@@ -206,22 +269,21 @@ const TeamComponent = () => {
               <Carousel.Item>
                 <Col className="m-b-30">
                   <Row className="no-gutters">
-                    <Col md="12" className="col-md-12 pro-pic t4">
+                    <Col md="12" className="col-md-12 pro-pic t6">
                       <div className="card-img-overlay">
                         <ul className="list-inline">
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-facebook"></i>
+                            <a
+                              href="http://lattes.cnpq.br/3712448942978626"
+                              target="_blank"
+                            >
+                              <i className="fa fa-linkedin"></i>
                             </a>
                           </li>
+
                           <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-twitter"></i>
-                            </a>
-                          </li>
-                          <li className="list-inline-item">
-                            <a href="#">
-                              <i className="fa fa-instagram"></i>
+                            <a href="mailto:daviatalgatti@gmail.com">
+                              <i className="fa fa-envelope"></i>
                             </a>
                           </li>
                         </ul>
@@ -229,8 +291,36 @@ const TeamComponent = () => {
                     </Col>
                     <Col md="12">
                       <div className="p-t-10">
-                        <h5 className="title font-medium">Coloborador 04</h5>
-                        <h6 className="subtitle">Especialidade em ...</h6>
+                        <h5 className="title font-medium">
+                          Dávia Marciana Talgatti
+                        </h5>
+                        <h6 className="subtitle">
+                          Possui graduação em Ciências Biológicas pela
+                          Universidade Federal de Pelotas (2006), mestrado em
+                          Biologia Vegetal pela Universidade Federal de Santa
+                          Catarina (2009), doutorado em Botânica pela
+                          Universidade Federal do Rio Grande do Sul (2014) e
+                          pós-doutorado em Sociedade, Natureza e Desenvolvimento ...
+                          {/* pela Universidade Federal do Oeste do Pará
+                          (2015-2017). Tem experiência na área de Botânica, com
+                          ênfase em Botânica criptogâmica, atuando
+                          principalmente nos seguintes temas: perifíton,
+                          fitoplâncton, microfitobentos, taxonomia, organismos
+                          bioindicadores da qualidade e condições ambientais de
+                          ecossistemas aquáticos, e ecologia de microalgas e
+                          cianobactérias. Atualmente é Professora Adjunta na
+                          área de Botânica, do curso de Ciências Biológicas do
+                          Campus de Oriximiná da Universidade Federal do Oeste
+                          do Pará e integra, como membro permanente, o PPG
+                          Biociências da mesma Universidade, onde é orientadora
+                          de mestrado. Ministra disciplinas de taxonomia,
+                          ecologia e fisiologia de algas e plantas amazônicas,
+                          atua orientando estudos que versam sobre a relação de
+                          algas, cianobactérias, plantas aquáticas e terrestres
+                          com as condições ambientais do meio que estão
+                          inseridas, levantamentos florísticos e ecologia de
+                          comunidades.{" "} */}
+                        </h6>
                       </div>
                     </Col>
                   </Row>
